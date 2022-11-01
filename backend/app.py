d7b30dd8ca7676268155c5ceb6f48ff2e24b525e
MAXIMILIANO LEONELO FRANCESCONI <maxifni_fsa@live.com.ar>
2022-11-01 15:33:13 -0300

    se agrego codigo de configuracion en el archivo app

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import git  # GitPython library
 
 app = Flask(__name__)
+
+
+# Route for the GitHub webhook
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./club-flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/')
 def index():
